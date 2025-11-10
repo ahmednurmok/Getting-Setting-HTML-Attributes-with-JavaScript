c5ea6525e167ed922b6183d948e95273d98e3ab7
ahmednurmok <ahmednurmok@gmail.com>
2025-11-10 18:41:09 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+
+//Task: Change Element attribute using JavaScript
+document.querySelector('a').attributes; 
+document.querySelector('a').getattribute('href');
+document.querySelector('a').setattribute('href', "bing.com");
+
